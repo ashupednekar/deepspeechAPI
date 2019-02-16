7ad2576f17bdcf409dc65e13c588dad9ff779411
ashupednekar <ashupednekar15@gmail.com>
2019-02-16 15:28:27 +0530

    added ds.pytorch support

--- deepspeechapi/__init__.py
+++ deepspeechapi/__init__.py
@@ -45,37 +45,25 @@ def decode_results(model, decoded_output, decoded_offsets):
     return results
 
 
-def transcribe(audio_path, parser, model, decoder, device):
-    spect = parser.parse_audio(audio_path).contiguous()
-    spect = spect.view(1, 1, spect.size(0), spect.size(1))
-    spect.to(device)
-    input_sizes = torch.IntTensor([spect.size(3)]).int()
-    out, output_sizes = model(spect, input_sizes)
-    decoded_output, decoded_offsets = decoder.decode(out, output_sizes)
-    return decoded_output, decoded_offsets
 
+parser = argparse.ArgumentParser(description='DeepSpeech transcription')
+parser = add_inference_args(parser)
+parser.add_argument('--audio-path', default='audio.wav',
+                    help='Audio file to predict on')
+parser.add_argument('--offsets', dest='offsets', action='store_true', help='Returns time offset information')
+parser = add_decoder_args(parser)
+args = parser.parse_args()
+device = torch.device("cuda" if args.cuda else "cpu")
+model = load_model(device, args.model_path, args.cuda)
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='DeepSpeech transcription')
-    parser = add_inference_args(parser)
-    parser.add_argument('--audio-path', default='audio.wav',
-                        help='Audio file to predict on')
-    parser.add_argument('--offsets', dest='offsets', action='store_true', help='Returns time offset information')
-    parser = add_decoder_args(parser)
-    args = parser.parse_args()
-    device = torch.device("cuda" if args.cuda else "cpu")
-    model = load_model(device, args.model_path, args.cuda)
+from deepspeechapi.decoder import BeamCTCDecoder
 
-    if args.decoder == "beam":
-        from decoder import BeamCTCDecoder
+decoder = BeamCTCDecoder(model.labels, lm_path=args.lm_path, alpha=args.alpha, beta=args.beta,
+                         cutoff_top_n=args.cutoff_top_n, cutoff_prob=args.cutoff_prob,
+                         beam_width=args.beam_width, num_processes=args.lm_workers)
+#decoder = GreedyDecoder(model.labels, blank_index=model.labels.index('_'))
 
-        decoder = BeamCTCDecoder(model.labels, lm_path=args.lm_path, alpha=args.alpha, beta=args.beta,
-                                 cutoff_top_n=args.cutoff_top_n, cutoff_prob=args.cutoff_prob,
-                                 beam_width=args.beam_width, num_processes=args.lm_workers)
-    else:
-        decoder = GreedyDecoder(model.labels, blank_index=model.labels.index('_'))
+parser = SpectrogramParser(model.audio_conf, normalize=True)
 
-    parser = SpectrogramParser(model.audio_conf, normalize=True)
-
-    decoded_output, decoded_offsets = transcribe(args.audio_path, parser, model, decoder, device)
-    print(json.dumps(decode_results(model, decoded_output, decoded_offsets)))
+#decoded_output, decoded_offsets = transcribe(args.audio_path, parser, model, decoder, device)
+#print(json.dumps(decode_results(model, decoded_output, decoded_offsets)))
--- deepspeechapi/views.py
+++ deepspeechapi/views.py
@@ -12,6 +12,16 @@ from .serializers import FileSerializer
 import scipy.io.wavfile as wav
 #from deepspeechapi import ds
 import json, os, time
+from deepspeechapi import decoder, parser, model, device, decode_results
+
+def transcribe(audio_path, parser, model, decoder, device):
+    spect = parser.parse_audio(audio_path).contiguous()
+    spect = spect.view(1, 1, spect.size(0), spect.size(1))
+    spect.to(device)
+    input_sizes = torch.IntTensor([spect.size(3)]).int()
+    out, output_sizes = model(spect, input_sizes)
+    decoded_output, decoded_offsets = decoder.decode(out, output_sizes)
+    return decoded_output, decoded_offsets
 
 class FileView(APIView):
 
@@ -29,8 +39,8 @@ class FileView(APIView):
             os.chdir('/home/ashu/Documents/bbuddy/dstorch/deepspeech.pytorch')
             transcript = json.loads(os.popen('python transcribe.py --model-path models/deepspeech_final.pth --audio-path ' + aud.split('.')[0] + '.wav' + ' --decoder beam --lm-path data/custom/text.binary ').read())['output'][0]['transcription']
             os.chdir('/home/ashu/Documents/bbuddy/deepspeechAPI/deepspeechapi/')
-
-            res = dict()
+            decoded_output, decoded_offsets = transcribe(aud.split('.')[0]+'.wav', parser, model, decoder, device)
+            res = decode_results(model, decoded_output, decoded_offsets)
             res["transcript"] = transcript
             print(time.time() - first)
             return Response(res, status=status.HTTP_201_CREATED)
